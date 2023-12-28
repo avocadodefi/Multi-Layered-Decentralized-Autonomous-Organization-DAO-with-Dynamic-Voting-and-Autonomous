6e5625a3bfc9691dca6723c579c6f41f2e82a250
0xDev <84038223+avocadodefi@users.noreply.github.com>
2023-12-28 08:24:39 +0100

    Update predictive-project-funding.sol
    his contract assumes the existence of a Chainlink Oracle providing market trend data (positive or negative). Projects can be added and funded. The shouldFundProject function gives a basic example of using external data to make funding decisions, although in a real-world scenario, this would be more complex.
    
    Remember, this is a highly simplified example. Real predictive project funding would require advanced algorithms and comprehensive data analysis, likely processed off-chain due to blockchain's computational and gas cost limitations. Always ensure thorough testing and consider security and efficiency implications when dealing with complex contract functionalities.

--- predictive-project-funding.sol
+++ predictive-project-funding.sol
@@ -1,1 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PredictiveProjectFunding {
+    AggregatorV3Interface internal marketDataFeed;
+
+    struct Project {
+        string name;
+        uint fundingGoal;
+        uint currentFunding;
+        bool funded;
+    }
+
+    Project[] public projects;
+    address public owner;
+
+    constructor(address _marketDataFeedAddress) {
+        marketDataFeed = AggregatorV3Interface(_marketDataFeedAddress);
+        owner = msg.sender;
+    }
+
+    function addProject(string memory _name, uint _fundingGoal) public {
+        require(msg.sender == owner, "Only owner can add projects");
+        projects.push(Project({
+            name: _name,
+            fundingGoal: _fundingGoal,
+            currentFunding: 0,
+            funded: false
+        }));
+    }
+
+    function fundProject(uint _projectId) public payable {
+        require(_projectId < projects.length, "Project does not exist");
+        Project storage project = projects[_projectId];
+        require(!project.funded, "Project already funded");
+        
+        project.currentFunding += msg.value;
+        if (project.currentFunding >= project.fundingGoal) {
+            project.funded = true;
+        }
+    }
+
+    function shouldFundProject(uint _projectId) public view returns (bool) {
+        require(_projectId < projects.length, "Project does not exist");
+        (,int marketTrend,,,) = marketDataFeed.latestRoundData();
+        return marketTrend > 0;
+    }
+}
